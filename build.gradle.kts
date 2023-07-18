b62bc7f9dea295e6ba2a587e8be81cb298f43ade
mfrancza <mfrancza@gmail.com>
2023-07-17 21:53:38 -0500

    Use jvmToolchain To Configure Java Version

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,12 +30,8 @@ dependencies {
     testImplementation(kotlin("test"))
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    jvmToolchain(11)
 }
 
 publishing {
